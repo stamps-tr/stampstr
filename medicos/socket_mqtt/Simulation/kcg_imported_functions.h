20d6baee4426194e17244169c33fddb9cf082216
Pedro de Castro <pedrobuenodecastro@gmail.com>
2017-09-20 09:06:15 -0300

    projeto socket mqtt

---
+++ medicos/socket_mqtt/Simulation/kcg_imported_functions.h
@@ -0,0 +1,20 @@
+/* $********** SCADE Suite KCG 32-bit 6.6 (build i19) ***********
+** Command: kcg66.exe -config C:/Users/Pedro/Documents/ITA/CE-235/socket_mqtt/Simulation/config.txt
+** Generation date: 2017-09-16T15:45:17
+*************************************************************$ */
+#ifndef _KCG_IMPORTED_FUNCTIONS_H_
+#define _KCG_IMPORTED_FUNCTIONS_H_
+
+#include "kcg_types.h"
+
+#ifndef PublishMQTT
+/* PublishMQTT/ */
+extern kcg_int32 PublishMQTT(/* Send/ */ kcg_bool Send);
+#endif /* PublishMQTT */
+
+#endif /* _KCG_IMPORTED_FUNCTIONS_H_ */
+/* $********** SCADE Suite KCG 32-bit 6.6 (build i19) ***********
+** kcg_imported_functions.h
+** Generation date: 2017-09-16T15:45:17
+*************************************************************$ */
+
---
+++ medicos/socket_mqtt/Simulation/socket_mqtt_interface.c
@@ -0,0 +1,181 @@
+#include "socket_mqtt_interface.h"
+#include "socket_mqtt_type.h"
+#include "socket_mqtt_mapping.h"
+#include "SmuVTable.h"
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const int  rt_version = Srtv62;
+
+const char* _SCSIM_CheckSum = "066f172040c5f64ecdadeb524c390f21";
+const char* _SCSIM_SmuTypesCheckSum = "70167ab69f4117fa3043a69f71c5aa42";
+
+/* context */
+
+outC_Operator outputs_ctx;
+
+static void _SCSIM_RestoreInterface(void) {
+    memset((void*)&outputs_ctx, 0, sizeof(outputs_ctx));
+}
+
+static void _SCSIM_ExecuteInterface(void) {
+    pSimulator->m_pfnAcquireValueMutex(pSimulator);
+    pSimulator->m_pfnReleaseValueMutex(pSimulator);
+}
+
+/* simulation */
+
+int SimInit(void) {
+    int nRet = 0;
+    _SCSIM_RestoreInterface();
+#ifdef EXTENDED_SIM
+    BeforeSimInit();
+#endif
+#ifndef KCG_USER_DEFINED_INIT
+    Operator_init(&outputs_ctx);
+    nRet = 1;
+#else
+    nRet = 0;
+#endif
+#ifdef EXTENDED_SIM
+    AfterSimInit();
+#endif
+    return nRet;
+}
+
+int SimReset(void) {
+    int nRet = 0;
+    _SCSIM_RestoreInterface();
+#ifdef EXTENDED_SIM
+    BeforeSimInit();
+#endif
+#ifndef KCG_NO_EXTERN_CALL_TO_RESET
+    Operator_reset(&outputs_ctx);
+    nRet = 1;
+#else
+    nRet = 0;
+#endif
+#ifdef EXTENDED_SIM
+    AfterSimInit();
+#endif
+    return nRet;
+}
+
+#ifdef __cplusplus
+    #ifdef pSimoutC_OperatorCIVTable_defined
+        extern struct SimCustomInitVTable *pSimoutC_OperatorCIVTable;
+    #else 
+        struct SimCustomInitVTable *pSimoutC_OperatorCIVTable = NULL;
+    #endif
+#else
+    struct SimCustomInitVTable *pSimoutC_OperatorCIVTable;
+#endif
+
+int SimCustomInit(void) {
+    int nRet = 0;
+    if (pSimoutC_OperatorCIVTable != NULL && 
+        pSimoutC_OperatorCIVTable->m_pfnCustomInit != NULL) {
+        /* VTable function provided => call it */
+        nRet = pSimoutC_OperatorCIVTable->m_pfnCustomInit ((void*)&outputs_ctx);
+    }
+    else {
+        /* VTable misssing => error */
+        nRet = 0;
+    }
+    return nRet;
+}
+
+#ifdef EXTENDED_SIM
+    int GraphicalInputsConnected = 1;
+#endif
+
+int SimStep(void) {
+#ifdef EXTENDED_SIM
+    if (GraphicalInputsConnected)
+        BeforeSimStep();
+#endif
+    _SCSIM_ExecuteInterface();
+    Operator(&outputs_ctx);
+#ifdef EXTENDED_SIM
+    AfterSimStep();
+#endif
+    return 1;
+}
+
+int SimStop(void) {
+#ifdef EXTENDED_SIM
+    ExtendedSimStop();
+#endif
+    return 1;
+}
+
+void SsmUpdateValues(void) {
+#ifdef EXTENDED_SIM
+    UpdateValues();
+#endif
+}
+
+void SsmConnectExternalInputs(int bConnect) {
+#ifdef EXTENDED_SIM
+    GraphicalInputsConnected = bConnect;
+#endif
+}
+
+/* dump */
+
+int SsmGetDumpSize(void) {
+    int nSize = 0;
+    nSize += sizeof(outC_Operator);
+#ifdef EXTENDED_SIM
+    nSize += ExtendedGetDumpSize();
+#endif
+    return nSize;
+}
+
+void SsmGatherDumpData(char * pData) {
+    char* pCurrent = pData;
+    memcpy(pCurrent, &outputs_ctx, sizeof(outC_Operator));
+    pCurrent += sizeof(outC_Operator);
+#ifdef EXTENDED_SIM
+    ExtendedGatherDumpData(pCurrent);
+#endif
+}
+
+void SsmRestoreDumpData(const char * pData) {
+    const char* pCurrent = pData;
+    memcpy(&outputs_ctx, pCurrent, sizeof(outC_Operator));
+    pCurrent += sizeof(outC_Operator);
+#ifdef EXTENDED_SIM
+    ExtendedRestoreDumpData(pCurrent);
+#endif
+}
+
+/* snapshot */
+
+int SsmSaveSnapshot(const char * szFilePath, size_t nCycle) {
+    /* Test Services API not available */
+    return 0;
+}
+
+int SsmLoadSnapshot(const char * szFilePath, size_t *nCycle) {
+    /* Test Services API not available */
+    return 0;
+}
+
+/* checksum */
+
+const char * SsmGetCheckSum() {
+    return _SCSIM_CheckSum;
+}
+
+const char * SsmGetSmuTypesCheckSum() {
+    return _SCSIM_SmuTypesCheckSum;
+}
+
+#ifdef __cplusplus
+} /* "C" */
+#endif
+
