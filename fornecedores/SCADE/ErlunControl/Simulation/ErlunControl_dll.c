d46696dee8f4cd8fe1a3b7b20214215183fdc74e
marcelopaivaramos <marcelopaivaramos@gmail.com>
2017-10-06 17:08:15 -0300

    Removendo
    Removendo

--- fornecedores/SCADE/ErlunControl/Simulation/ErlunControl_dll.c
+++
@@ -1,2 +0,0 @@
-const char*  pszDllPathname = "ErlunControl.dll";
-const char*  pszLauncherPathname = "C:/Program Files/ANSYS Inc/v180/SCADE/SCADE/bin/SCSSMLNC.exe";
--- fornecedores/SCADE/ErlunControl/Simulation/ErlunControl_mapping.h
+++
@@ -1,1 +0,0 @@
-/* ErlunControl_mapping.h */
