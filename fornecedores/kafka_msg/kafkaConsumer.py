d6b12530c61ed7ee2a9aa80d812f2345db7c0f9b
marcelopaivaramos <marcelopaivaramos@gmail.com>
2017-10-11 11:54:00 -0300

    kafka
    Atualizando código para o kafka Producer e Consumer para fotos (base64) e mensagens de texto simples...

---
+++ fornecedores/kafka_msg/kafkaConsumer.py
@@ -0,0 +1,28 @@
+# Autor: Marcelo Paiva
+# Data: 06/10/2017
+# Objetivo: Escutar o Kafka no topico: erlun
+
+import sys
+from kafka import KafkaConsumer
+
+# Programa principal
+if __name__ == "__main__":
+
+    # Help
+    if len(sys.argv) != 1:
+        print("Usage: kafkaConsumer.py")
+        exit(-1)
+
+    # Inicializa variaveis
+    broker = "stampsnet.hashtagsource.com:9092"
+    topico = "erlun"
+
+    # Conecta no servidor Kafka
+    consumer = KafkaConsumer(bootstrap_servers=broker)
+
+    # Acessa o topico
+    consumer.subscribe([topico])
+
+    # Imprime somente as mensagens novas
+    for message in consumer:
+        print (message.value)
