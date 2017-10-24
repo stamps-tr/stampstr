5f6bfa9ad3306fb43df0b2d2a72e602daeabbc5c
Alyson <alyson.basilio@delfosim.com>
2017-10-24 19:58:29 -0200

    Classificador pronto

--- pacientes/NLTK/scriptCreateNLTKClassifier.py
+++ pacientes/NLTK/scriptCreateNLTKClassifier.py
@@ -1,12 +1,28 @@
 import nltk
 import pickle
 from pacientes.NLTK.classifier import Filtering
+import csv
+import pprint
 
+dic = {}
+with open('dataset_clean1.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        if row[0] in dic:
+            dic[row[0]] += ", "+row[1]
+        else:
+            dic[row[0]]=row[1]
+list =[]
+for doenca in dic.keys():
+    list.append([doenca,dic[doenca]])
+# pprint.pprint(dic)
+# pprint.pprint(list)
 filter=Filtering()
 print("Preparing set to training")
 trainSet=filter.preparingSetToTrain(list,10000)
-# print(trainSet)
+pprint.pprint(trainSet)
 classifier = nltk.NaiveBayesClassifier.train(trainSet)
+
 print("Saving classifier")
 f = open('my_classifier.pickle', 'wb')
 pickle.dump(classifier, f)
